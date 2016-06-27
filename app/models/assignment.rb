55edf5d9ca72fdf3b92a1556ddce4a67674dae34
ellismiskell <ellis.burgoonmiskell@yale.edu>
2016-06-27 10:53:23 -0400

    task

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,11 +3,11 @@ class Assignment < ActiveRecord::Base
   belongs_to :user
 
   def self.send_notifs
-    a = self.where("date_due between ? and ?", Time.now, Time.now + 12.hours).where(reminder_type: 'homework')
+    a = Assignment.where("date_due between ? and ?", Time.now, Time.now + 12.hours).where(reminder_type: 'homework')
     a.each do |b|
       NotificationMailer.send_reminder_email.deliver_now
     end
-    c = self.where("date_due between ? and ?", Time.now, Time.now + 24.hours).where(reminder_type: 'bring')
+    c = Assignment.where("date_due between ? and ?", Time.now, Time.now + 24.hours).where(reminder_type: 'bring')
     c.each do |d|
       NotificationMailer.send_reminder_email.deliver_now
     end
